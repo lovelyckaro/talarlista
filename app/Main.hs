627b1c03d142f9f9e65805d0adc18415580adeec
Love Lyckaro <love.lyckaro@hotmail.se>
2021-10-04 16:41:38 +0200

    pretty

--- app/Main.hs
+++ app/Main.hs
@@ -34,6 +34,9 @@ clearPerson (TalarLista [] [] set) = TalarLista [] [] set
 removeName :: Person -> TalarLista -> TalarLista
 removeName person (TalarLista l1 l2 set) = TalarLista (SQ.filter (/= person) l1) (SQ.filter (/= person) l2) set
 
+suc :: Person -> Map Person AmountSpoken -> Map Person AmountSpoken
+suc person = M.insertWith (+) person 1 
+
 handleInput :: String -> TalarListor -> TalarListor
 handleInput "push" (TalarListor lists gstats) = TalarListor (emptyList : lists) gstats
 handleInput "pop" (TalarListor [] gstats) = TalarListor [emptyList] gstats
@@ -44,11 +47,9 @@ handleInput "clear" ls = handleInput "x" ls
 handleInput ('x' : ' ' : name) (TalarListor (list : rest) gstats) = TalarListor (removeName name list : rest) gstats
 handleInput "n" _ = empty
 handleInput "new" l = handleInput "n" l
-handleInput (p : erson) (TalarListor (list : rest) gstats) =
-  let person = toUpper p : erson
-   in if person `member` talat list
-        then TalarListor (list {lista2 = lista2 list |> person} : rest) (M.insertWith (+) person 1 gstats)
-        else TalarListor (list {lista1 = lista1 list |> person} : rest) (M.insertWith (+) person 1 gstats)
+handleInput person (TalarListor (list : rest) gstats)
+  | person `member` talat list = TalarListor (list {lista2 = lista2 list |> person} : rest) (suc person gstats)
+  | otherwise =  TalarListor (list {lista1 = lista1 list |> person} : rest) (suc person gstats)
 
 emptyList :: TalarLista
 emptyList = TalarLista [] [] S.empty
@@ -56,8 +57,12 @@ emptyList = TalarLista [] [] S.empty
 empty :: TalarListor
 empty = TalarListor [emptyList] M.empty
 
+nameCap :: String -> String
+nameCap [] = []
+nameCap (p:erson) = toUpper p : erson
+
 printList :: TalarLista -> String
-printList TalarLista {..} = unlines (toList $ lista1 <> lista2)
+printList TalarLista {..} = unlines (toList (nameCap <$> (lista1 <> ["—————————————————————————"] <>  lista2)))
 
 printListor :: TalarListor -> String
 printListor (TalarListor [] _) = error "Något har gått riktigt fel"
